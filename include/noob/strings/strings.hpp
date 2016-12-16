6dd30c56ac9c86608d66ca6d58626cfe21a2cc34
Colin <colingilbert86@gmail.com>
2016-12-15 18:18:41 -0600

    Add another UTF8 checker

--- include/noob/strings/strings.hpp
+++ include/noob/strings/strings.hpp
@@ -164,6 +164,8 @@ namespace noob
 			return result;
 		}
 
+	// NOTE: utf8_check() made by Markus Kuhn <http://www.cl.cam.ac.uk/~mgk25/> -- 2005-03-30. Info below:
+
 	/*
 	 * The utf8_check() function scans the '\0'-terminated string starting
 	 * at s. It returns a pointer to the first byte of the first malformed
@@ -226,4 +228,68 @@ namespace noob
 	}
 
 
-}
+
+	// Copyright (c) 2008-2009 Bjoern Hoehrmann <bjoern@hoehrmann.de>
+	// Copyright (c) 2008-2009 Bjoern Hoehrmann <bjoern@hoehrmann.de>
+	// See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
+	//
+	// Permission is hereby granted, free of charge, to any person obtaining a 
+	// copy of this software and associated documentation files (the "Software"),
+	// to deal in the Software without restriction, including without limitation
+	// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	// and/or sell copies of the Software, and to permit persons to whom the
+	// Software is furnished to do so, subject to the following conditions:
+	//
+	// The above copyright notice and this permission notice shall be included
+	// in all copies or substantial portions of the Software.
+	//
+	// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+	// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+	// DEALINGS IN THE SOFTWARE.
+
+#include "utf8.h"
+
+#define UTF8_ACCEPT 0
+#define UTF8_REJECT 1
+
+
+	static const uint8_t s_utf8d[364] =
+	{
+		// The first part of the table maps bytes to character classes that
+		// to reduce the size of the transition table and create bitmasks.
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,  9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
+		7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,  7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+		8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+		10,3,3,3,3,3,3,3,3,3,3,3,3,4,3,3, 11,6,6,6,5,8,8,8,8,8,8,8,8,8,8,8,
+
+		// The second part is a transition table that maps a combination
+		// of a state of the automaton and a character class to a state.
+		0,12,24,36,60,96,84,12,12,12,48,72, 12,12,12,12,12,12,12,12,12,12,12,12,
+		12, 0,12,12,12,12,12, 0,12, 0,12,12, 12,24,12,12,12,12,12,24,12,24,12,12,
+		12,12,12,12,12,12,12,24,12,12,12,12, 12,24,12,12,12,12,12,12,12,24,12,12,
+		12,12,12,12,12,12,12,36,12,36,12,12, 12,36,12,12,12,12,12,36,12,36,12,12,
+		12,36,12,12,12,12,12,12,12,12,12,12
+	};
+
+	static uint32_t utf8_decode(uint32_t* _state, uint32_t* _codep, uint8_t _ch)
+	{
+		uint32_t byte = _ch;
+		uint32_t type = s_utf8d[byte];
+
+		*_codep = (*_state != UTF8_ACCEPT) ?
+			(byte & 0x3fu) | (*_codep << 6) :
+			(0xff >> type) & (byte);
+
+		*_state = s_utf8d[256 + *_state + type];
+		return *_state;
+	}
+	//
+
